a72a65f992b4b8c9caa892555f3091b2cce75f9b
johnnyalvesx <alvesjohnny@outlook.com>
2023-10-24 18:14:24 -0300

    Swagger configurado

--- src/main/java/br/com/generationbrasil/studentsgen/StudentsgenApplication.java
+++ src/main/java/br/com/generationbrasil/studentsgen/StudentsgenApplication.java
@@ -1,9 +1,13 @@
 package br.com.generationbrasil.studentsgen;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Students Gen Swagger", version = "1",
+		description = "API RESTful para cadastro de alunos e suas notas semestrais"))
 public class StudentsgenApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/generationbrasil/studentsgen/controllers/StudentController.java
+++ src/main/java/br/com/generationbrasil/studentsgen/controllers/StudentController.java
@@ -3,6 +3,10 @@ package br.com.generationbrasil.studentsgen.controllers;
 import br.com.generationbrasil.studentsgen.dtos.StudentRecordDto;
 import br.com.generationbrasil.studentsgen.models.StudentModel;
 import br.com.generationbrasil.studentsgen.repositories.StudentRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +19,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@Tag(name = "students-gen")
 public class StudentController {
 
     @Autowired
     StudentRepository studentRepository;
 
+    @Operation(description = "Cadastra um aluno e suas respectivas notas semestrais. " +
+            "Deve-se preencher todos os campos.")
     @PostMapping("/students")
     public ResponseEntity<StudentModel> saveStudent(@RequestBody @Valid StudentRecordDto studentRecordDto) {
         var studentModel = new StudentModel();
@@ -27,11 +34,20 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(studentRepository.save(studentModel));
     }
 
+    @Operation(description = "Retorna todos os alunos cadastrados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de alunos retornada com sucesso.")
+    })
     @GetMapping("/students")
     public ResponseEntity<List<StudentModel>> getAllStudents() {
         return ResponseEntity.status(HttpStatus.OK).body(studentRepository.findAll());
     }
 
+    @Operation(description = "Busca o aluno pelo id. Deve ser informado o valor UUID gerado no cadastro.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aluno encontrado no sistema."),
+            @ApiResponse(responseCode = "404", description = "Aluno não encontrado no sistema.")
+    })
     @GetMapping("/students/{idStudent}")
     public ResponseEntity<Object> getOneStudent(@PathVariable(value = "idStudent") UUID idStudent) {
         Optional<StudentModel> student = studentRepository.findById(idStudent);
@@ -41,6 +57,13 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.OK).body(student.get());
     }
 
+    @Operation(description = "Atualiza o cadastro de um aluno. " +
+            "Deve ser informado o valor UUID gerado no cadastro.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aluno encontrado atualizado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Aluno não encontrado no sistema."),
+            @ApiResponse(responseCode = "400", description = "Informar todos os dados do body.")
+    })
     @PutMapping("/students/{idStudent}")
     public ResponseEntity<Object> updateStudent(@PathVariable(value = "idStudent") UUID idStudent,
                                                 @RequestBody @Valid StudentRecordDto studentRecordDto) {
@@ -53,6 +76,12 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.OK).body(studentRepository.save(studentModel));
     }
 
+    @Operation(description = "Deleta o cadastro de um aluno. " +
+            "Deve ser informado o valor UUID gerado no cadastro.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Aluno deletado do sistema com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Aluno não encontrado no sistema."),
+    })
     @DeleteMapping("/students/{idStudent}")
     public ResponseEntity<Object> deleteStudent(@PathVariable(value = "idStudent") UUID idStudent) {
         Optional<StudentModel> student = studentRepository.findById(idStudent);
