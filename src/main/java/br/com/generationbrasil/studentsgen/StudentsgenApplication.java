4e6d784769503c372ee0f31433f080bd8d33bcf5
johnnyalvesx <alvesjohnny@outlook.com>
2023-10-24 21:26:37 -0300

    Alteração na aplicação principal

--- src/main/java/br/com/generationbrasil/studentsgen/StudentsgenApplication.java
+++ src/main/java/br/com/generationbrasil/studentsgen/StudentsgenApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="br.com.generationbrasil.studentsgen.repositories")
 @OpenAPIDefinition(info = @Info(title = "Students Gen Swagger", version = "1",
 		description = "API RESTful para cadastro de alunos e suas notas semestrais"))
 public class StudentsgenApplication {
